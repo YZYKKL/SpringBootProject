ef9b9d032fe8c27a01fc0ce024e345c248ead749
YZYKKL <121049659@qq.com>
2023-07-17 16:28:55 +0800

    test

---
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.backend.controller;
+
+import com.example.backend.entity.RestBean;
+import com.example.backend.entity.user.AccountUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/me")
+    public RestBean<AccountUser> me(@SessionAttribute("account") AccountUser user){
+        return RestBean.success(user);
+    }
+}
